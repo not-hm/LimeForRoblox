93e09850541f1063be8ba904defa84f620571886
nothm_ <127741557+not-hm@users.noreply.github.com>
2025-11-15 12:39:25 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 repeat task.wait() until game:IsLoaded()
-if game.PlaceId == 11630038968 or game.PlaceId == 10810646982 then
+if game.PlaceId == 11630038968 or game.PlaceId == 10810646982 or game.PlaceId == 139566161526375 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/not-hm/LimeForRoblox/refs/heads/main/bridge_duel.lua"))()
 else
 	game:GetService("Chat"):Chat(game.Players.LocalPlayer.Character:WaitForChild("Head"), "I love eating AquaVClip feces")
