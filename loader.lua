2060ef312c03bf94c31e8814ab230894cf34b8f6
nothm_ <127741557+not-hm@users.noreply.github.com>
2025-11-23 15:17:19 +0700

    Lime reborn

---
+++ loader.lua
@@ -0,0 +1,6 @@
+repeat task.wait() until game:IsLoaded()
+if game.PlaceId == 11630038968 or game.PlaceId == 10810646982 or game.PlaceId == 139566161526375 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/not-hm/LimeForRoblox/refs/heads/main/bridge_duel.lua"))()
+else
+	game:GetService("Chat"):Chat(game.Players.LocalPlayer.Character:WaitForChild("Head"), "I love eating AquaVClip feces")
+end
