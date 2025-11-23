a669035508b0eb052ab0eff120aa0f344d605f47
nothm_ <127741557+not-hm@users.noreply.github.com>
2025-11-23 15:15:22 +0700

    Delete loader.lua

--- loader.lua
+++
@@ -1,8 +0,0 @@
-repeat task.wait() until game:IsLoaded()
-
-return game:GetService("Chat"):Chat(game.Players.LocalPlayer.Character:WaitForChild("Head"), "I love eating AquaVClip feces and I tried to use Lime.")
---[[if game.PlaceId == 11630038968 or game.PlaceId == 10810646982 or game.PlaceId == 139566161526375 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/not-hm/LimeForRoblox/refs/heads/main/bridge_duel.lua"))()
-else
-	game:GetService("Chat"):Chat(game.Players.LocalPlayer.Character:WaitForChild("Head"), "I love eating AquaVClip feces")
-end]]
